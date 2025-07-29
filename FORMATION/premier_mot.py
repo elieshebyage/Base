9f13f0355904b1dbcfa5b795f7a6797b5186138c
elieshebyage <eshebyage@gmail.com>
2025-07-29 14:30:11 +0100

    ajout premier mot

---
+++ FORMATION/premier_mot.py
@@ -0,0 +1,4 @@
+''' Ecrire un programme en python, qui permet de renvoyer le premier mot d'un text donné '''
+texte=input("entrer le text : ")
+t=texte.split()
+print("le premier mot du texte est :" ,t[0])
